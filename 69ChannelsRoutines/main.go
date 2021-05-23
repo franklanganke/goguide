eae4d9f4b262f9bdaf6e21dc62bc3078cf01a04a
Frank Langanke <frank.langanke@firedrill.de>
2021-05-23 16:47:34 +0200

    refactor to anonymous func

--- 69ChannelsRoutines/main.go
+++ 69ChannelsRoutines/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,11 +21,15 @@ func main() {
 	}
 
 	for l := range c {
-		go checkLink(l, c)
+		go func(link string) {
+			time.Sleep(time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 }
 
 func checkLink(link string, c chan string) {
+	time.Sleep(time.Second)
 	_, err := http.Get(link)
 	if err != nil {
 
